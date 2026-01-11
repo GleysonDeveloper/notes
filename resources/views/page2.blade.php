8d4bd524417169eb8729b02bef9e85d1056be1f6
GleysonDeveloper <gleysonandrade@mundoinfo.info>
2026-01-11 11:01:54 -0300

    006

--- resources/views/page2.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main_layout')
-
-@section('content')
-    <h1>Welcome View and Blade</h1>
-    <hr>
-    <h3>Page 2</h3>
-    <h4>The value is: {{ $value }}</h4>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,3 @@
 
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('main');
-});
-
-Route::get('/about', function () {
-    echo 'About us';
-});
-
-// Rota com parâmetros
-Route::get('/main/{value}', [MainController::class, 'index']);
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-Route::get('/page3/{value}', [MainController::class, 'page3']);
