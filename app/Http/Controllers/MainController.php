f203a4fc24c72387b29975f13a1efb4e5e2dc597
GleysonDeveloper <gleysonandrade@mundoinfo.info>
2026-01-20 20:13:26 -0300

    36

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -14,9 +14,11 @@ class MainController extends Controller
         // load user's notes
         $id = session('user.id');
         $user = User::find($id)->toArray();
-        $notes = User::find($id)->notes()->get()->toArray();
-
-
+        $notes = User::find($id)
+                    ->notes()
+                    ->get()
+                    ->whereNull('deleted_at')
+                    ->toArray();
 
         // show home view
         return view('home', ['notes' => $notes]);
@@ -119,6 +121,30 @@ class MainController extends Controller
     public function deleteNote($id)
     {
         $id = Operations::decryptId($id);
-        echo "I'm delete note with ID: " . $id;
+        
+        // load note
+        $note = Note::find($id);
+
+        // show delete note confirmation
+        return view('delete_note', ['note' => $note]);
+    }
+
+    public function deleteNoteConfirm($id)
+    {
+        // check if $id is encrypted
+        $id = Operations::decryptId($id);
+
+        // load note
+        $note = Note::find($id);
+
+        // hard delete
+        // $note->delete();
+
+        // soft delete
+        $note->deleted_at = date('Y:m:d H:i:s');
+        $note->save();
+
+        // redirect to home
+        return redirect()->route('home');
     }
 }
